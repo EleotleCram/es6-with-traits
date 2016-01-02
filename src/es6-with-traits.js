6fdc7d5a684a66b8aa769a9db3f4873e1031350a
Marcel Toele <EleotleCram@gmail.com>
2016-01-02 10:57:25 +0100

    src/es6-with-traits.js: Added checking mechanism to see if an object mimics a trait

--- src/es6-with-traits.js
+++ src/es6-with-traits.js
@@ -1,12 +1,45 @@
 class Base {}
 const Trait = function(traitFunc) {
 	traitFunc.__trait__ = true;
+	let iface = getInterface(traitFunc);
+	traitFunc.mimics = traitFunc.check = iface.mimics;
+
 	return traitFunc;
 };
 const withTraits = function(baseClassOrFirstTrait, ...traits) {
 	var baseClass = baseClassOrFirstTrait.__trait__ ? baseClassOrFirstTrait(Base) : baseClassOrFirstTrait;
 	return traits.reduce((parentClass, trait) => trait(parentClass), baseClass);
 }
+
+const getInterface = function(trait) {
+	var aClass = trait(Base);
+	var instance = new aClass();
+	var prototype = Object.getPrototypeOf(instance);
+	var propertyNames = Object.keys(instance)
+						.concat(Object.getOwnPropertyNames(prototype)
+						.concat(Object.getOwnPropertySymbols(prototype)));
+
+	var typeString = (x) => Object.prototype.toString.call(x);
+
+	var propertiesRequiredByInterface = [];
+	for (let name of propertyNames) {
+		let property = instance[name];
+		if(property !== aClass) {
+			propertiesRequiredByInterface.push({name, type: typeString(property)});
+		}
+	}
+
+	// Check if object mimics this interface (has all the properties required by the interface)
+	var mimics = function(object) {
+		return propertiesRequiredByInterface.every((p) => typeString(object[p.name]) === p.type);
+	}
+
+	return {
+		mimics,
+		check: mimics, // compatibility with methodical
+	};
+}
+
 
 if (typeof module !== 'undefined') {
 	module.exports = {
@@ -61,13 +94,16 @@ if (require.main === module) {
 
 	class Dog extends withTraits(Animal, Quadruped, Walking) {}
 	class Bird extends withTraits(Animal, Biped, Walking, Flying) {}
-
 	let dog = new Dog();
 	console.log("dog.walk():");
 	dog.walk();
+
 	let bird = new Bird();
 	console.log("bird.walk():");
 	bird.walk();
 	console.log("bird.fly():");
 	bird.fly();
+
+	console.log("Does a dog walk?", Walking.mimics(dog)); // => true
+	console.log("Does a dog fly?", Flying.mimics(dog)); // => false
 }
